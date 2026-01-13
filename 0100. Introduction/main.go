96e7b1d4b996c174c4b318801cbdb027a8a76225
Dmitry Utropov <dmitry.wu@gmail.com>
2026-01-13 04:49:04 +0300

    add temp 0100

--- 0100. Introduction/main.go
+++ 0100. Introduction/main.go
@@ -2,9 +2,24 @@
 
 import "fmt"
 
+type Notifier interface {
+	Notify (message string)	
+}
+
+type EmailSender struct {}
+
+func (e EmailSender) Notify(message string){
+	fmt.Println("Отправка email:", message)
+}
+
+func Send(n Notifier) {
+n.Notify("Добро пожаловать")
+}
+
 func main() {
 	fmt.Print("\033[H\033[2J")
 
+	Send(EmailSender{})
 
 
 }
